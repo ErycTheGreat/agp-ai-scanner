2d931cb3c065db03313fad44383df53576df4d4d
Eryc Tri Juni S <147351364+ErycTheGreat@users.noreply.github.com>
2026-06-08 23:02:08 +0700

    Change CSS selector from body to html

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ async function extractPayload(env) {
 		await env.AGP_STATE.put("LCP_IMAGE_URL", "/assets/image/hero.avif");
 
         // 2. Build the CSS and save to KV
-        const safeCss = `body { background-color: ${parsedData.bgColor} !important; } .ghost-skeleton { width: 100vw; height: 100vh; background-color: ${parsedData.bgColor}; }`;
+        const safeCss = `html { background-color: ${parsedData.bgColor} !important; } .ghost-skeleton { width: 100vw; height: 100vh; background-color: ${parsedData.bgColor}; }`;
         await env.AGP_STATE.put("GHOST_CSS", safeCss);
         
         console.log("AGP State Updated Successfully in KV.");
